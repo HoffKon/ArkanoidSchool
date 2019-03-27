a7042132c8adbef38bd7c27da62169b7ae1f34ae
k0rku <k0rku.yt@gmail.com>
2019-03-27 12:47:42 +0100

    ball move

--- Mathss/Assets/Scripts/Ball.cs
+++ Mathss/Assets/Scripts/Ball.cs
@@ -1,17 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 
 public class Ball : MonoBehaviour {
 
     [SerializeField] PaddleMovement paddle1;
+    [SerializeField] float xPush;
+    [SerializeField] float yPush;
+    [SerializeField] bool hasStarted = false;
+    [SerializeField] Rigidbody2D rb;
     Vector2 paddleToBallVector;
-    void Start()
+
+    private void Start()
     {
         paddleToBallVector = transform.position - paddle1.transform.position;
+        rb = GetComponent<Rigidbody2D>();
+        rb.simulated = false;
+
     }
-    void Update ()
+    private  void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallPaddle();
+            LaunchToPaddle();
+        }
+    }
+    private void LaunchToPaddle()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            rb.simulated = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+    
+    private void LockBallPaddle()
     {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
